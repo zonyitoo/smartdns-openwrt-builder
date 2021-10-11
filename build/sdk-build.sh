7a1110491a32466c2ff4f025dcf90c86421d0adf
zonyitoo <zonyitoo@gmail.com>
2021-10-12 02:53:15 +0800

    print ./bin owners

--- build/sdk-build.sh
+++ build/sdk-build.sh
@@ -2,11 +2,14 @@
 
 cd /home/build/openwrt
 
+ls -alh ./bin
+
+echo 'BUILD START'
+
 make defconfig
 if [[ "$?" != "0" ]]; then
     exit $?
 fi
-
 
 ./scripts/feeds update && ./scripts/feeds install libpthread libopenssl
 if [[ "$?" != "0" ]]; then
